70f80f3bfe79eb6c118410a6aa6db2b4c7c35109
WingedLionGH <martin@emagmedia.de>
2025-06-18 23:42:28 +0200

    fix

---
+++ custom_components/bee_todo/bee_todo_panel.js
@@ -0,0 +1,7 @@
+class BeeTodoPanel extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<h1>Bee Todo</h1><p>Loaded!</p>`;
+  }
+}
+customElements.define("bee-todo-panel", BeeTodoPanel);
+document.body.innerHTML = "<bee-todo-panel></bee-todo-panel>";
