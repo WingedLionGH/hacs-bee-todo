f6cd51dd32ff756d1f25cce720e2e51dc4330c0b
WingedLionGH <martin@emagmedia.de>
2025-06-18 21:52:44 +0200

    add

--- custom_components/bee_todo/__init__.py
+++ custom_components/bee_todo/__init__.py
@@ -9,4 +9,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN] = manager
 
     hass.http.register_view(TodoApiView(hass, manager))
+    
+    hass.components.frontend.async_register_built_in_panel(
+    component_name="custom",
+    sidebar_title="Bee Todo",
+    sidebar_icon="mdi:format-list-checkbox",
+    frontend_url_path="bee_todo",
+    config={"name": "bee_todo_panel"},
+    require_admin=False,
+    )
+    
     return True
